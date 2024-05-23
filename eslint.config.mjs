7ad22e5ff861869df69ac1910e7ff21cbebd1e96
조석규 <ezaurum@gmail.com>
2024-05-24 00:10:42 +0900

    chore: Update lint version to latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReactConfigJSX from "eslint-plugin-react/configs/jsx-runtime.js";
+import {fixupConfigRules} from "@eslint/compat";
+
 export default [
+    {
+        ignores: ["*.d.ts", "dist/*"],
+    },
+    {
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    {
+        languageOptions: {globals: globals.browser},
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...fixupConfigRules(pluginReactConfig),
+    ...fixupConfigRules(pluginReactConfigJSX),
 ];
