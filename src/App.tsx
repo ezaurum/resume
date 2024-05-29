b14850adeb349432e24b663d0a4b0545b7b92604
조석규 <ezaurum@gmail.com>
2024-05-29 13:46:04 +0900

    feat: Add focus animation for input

--- src/App.tsx
+++ src/App.tsx
@@ -37,57 +37,75 @@ function App() {
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}`
   }
 
+  const [isBig, setIsBig] = useState(false)
+
   return (
-    <main className="p-2 flex flex-col gap-4">
-      <h1>Resume</h1>
-      <h2>{profile.fullName}</h2>
-      <LinkSection />
-      <input className="p-2 border-2" type={"text"} onInput={inputFilterText} />
+    <main className="p-2 flex flex-col gap-4 relative">
+      <div className="absolute h-28 left-2 top-2 right-2 flex flex-col gap-6 items-center justify-center">
+        <h1
+          className={`
+        transition-all duration-200 ease-linear 
+        ${isBig ? `text-xl` : `text-4xl`}`}
+        >
+          어떤 사람을 찾고 계시나요?
+        </h1>
+        <input
+          className={`w-full box-border transition-all duration-200 ease-linear border-2 ${isBig ? "text-4xl p-4" : "text-xl p-2"}`}
+          type={"text"}
+          onInput={inputFilterText}
+          onFocus={() => setIsBig(true)}
+          onBlur={() => setIsBig(false)}
+        />
+      </div>
+      <div className="mt-32">
+        <h2>{profile.fullName}</h2>
+        <LinkSection />
 
-      {projectSearchResult.map((item) => (
-        <div key={item.title}>
-          <h3>{item.title}</h3>
-          <p>{item.description}</p>
+        {projectSearchResult.map((item) => (
+          <div key={item.title}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
 
-          {item.to || item.from ? (
-            <p>
-              <span>기간: </span>
-              {item.from ? <span>{toYearMonth(item.from)}</span> : null}~
-              {item.to ? <span>{toYearMonth(item.to)}</span> : "현재"}~
-            </p>
-          ) : null}
-          {item.employer ? <div>업체: {item.employer}</div> : null}
+            {item.to || item.from ? (
+              <p>
+                <span>기간: </span>
+                {item.from ? <span>{toYearMonth(item.from)}</span> : null}~
+                {item.to ? <span>{toYearMonth(item.to)}</span> : "현재"}~
+              </p>
+            ) : null}
+            {item.employer ? <div>업체: {item.employer}</div> : null}
 
-          {item.teamSize ? <div>팀 규모: {item.teamSize} 인</div> : null}
-          {item.roles ? (
+            {item.teamSize ? <div>팀 규모: {item.teamSize} 인</div> : null}
+            {item.roles ? (
+              <div>
+                역할:
+                <ul className="flex flex-row gap-2">
+                  {item.roles.map((i) => Tag(i))}
+                </ul>
+              </div>
+            ) : null}
             <div>
-              역할:
+              기술 스택:
               <ul className="flex flex-row gap-2">
-                {item.roles.map((i) => Tag(i))}
+                {item.tags.map((i) => Tag(i))}
               </ul>
             </div>
-          ) : null}
-          <div>
-            기술 스택:
-            <ul className="flex flex-row gap-2">
-              {item.tags.map((i) => Tag(i))}
-            </ul>
+            <div>
+              성과:
+              <ul className="flex flex-col gap-2">
+                {item.achievements.map((tag) => (
+                  <li
+                    className="before:content-['•'] before:px-1 before:font-bold"
+                    key={tag}
+                  >
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div>
-            성과:
-            <ul className="flex flex-col gap-2">
-              {item.achievements.map((tag) => (
-                <li
-                  className="before:content-['•'] before:px-1 before:font-bold"
-                  key={tag}
-                >
-                  {tag}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </main>
   )
 }
